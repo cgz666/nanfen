41bd69829da2a97b82c0b1d612804391d885c418
cgz666 <275694332@qq.com>
2026-03-11 11:39:25 +0800

    Signed-off-by: cgz666 <275694332@qq.com>

--- spider/script/down_foura/foura_spider_universal.py
+++ spider/script/down_foura/foura_spider_universal.py
@@ -1,4 +1,7 @@
+import sys
 import os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+sys.path.insert(0, root_dir)
 import pickle
 import schedule
 import time
